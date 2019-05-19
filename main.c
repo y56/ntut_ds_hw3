c2ade0ed07cca7bcce218189dd83e828db7013eb
y56 <36190560+y56@users.noreply.github.com>
2019-05-19 17:25:25 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,92 +1,84 @@
-// C program for array implementation of queue 
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <limits.h> 
+#include <iostream>
+using namespace std;
 
-#include "ds.h"
-  
-// A structure to represent a queue 
-struct Queue 
-{ 
-    int front, rear, size; 
-    unsigned capacity; 
-    int* array; 
-}; 
-  
-// function to create a queue of given capacity.  
-// It initializes size of queue as 0 
-struct Queue* createQueue(unsigned capacity) 
-{ 
-    struct Queue* queue = (struct Queue*) malloc(sizeof(struct Queue)); 
-    queue->capacity = capacity; 
-    queue->front = queue->size = 0;  
-    queue->rear = capacity - 1;  // This is important, see the enqueue 
-    queue->array = (int*) malloc(queue->capacity * sizeof(int)); 
-    return queue; 
-} 
-  
-// Queue is full when size becomes equal to the capacity  
-int isFull(struct Queue* queue) 
-{  return (queue->size == queue->capacity);  } 
-  
-// Queue is empty when size is 0 
-int isEmpty(struct Queue* queue) 
-{  return (queue->size == 0); } 
-  
-// Function to add an item to the queue.   
-// It changes rear and size 
-void enqueue(struct Queue* queue, int item) 
-{ 
-    if (isFull(queue)) 
-        return; 
-    queue->rear = (queue->rear + 1)%queue->capacity; 
-    queue->array[queue->rear] = item; 
-    queue->size = queue->size + 1; 
-    printf("%d enqueued to queue\n", item); 
-} 
-  
-// Function to remove an item from queue.  
-// It changes front and size 
-int dequeue(struct Queue* queue) 
-{ 
-    if (isEmpty(queue)) 
-        return INT_MIN; 
-    int item = queue->array[queue->front]; 
-    queue->front = (queue->front + 1)%queue->capacity; 
-    queue->size = queue->size - 1; 
-    return item; 
-} 
-  
-// Function to get front of queue 
-int front(struct Queue* queue) 
-{ 
-    if (isEmpty(queue)) 
-        return INT_MIN; 
-    return queue->array[queue->front]; 
-} 
-  
-// Function to get rear of queue 
-int rear(struct Queue* queue) 
-{ 
-    if (isEmpty(queue)) 
-        return INT_MIN; 
-    return queue->array[queue->rear]; 
-} 
-  
-// Driver program to test above functions./ 
-int main() 
-{ 
-    struct Queue* queue = createQueue(1000); 
-  
-    enqueue(queue, 10); 
-    enqueue(queue, 20); 
-    enqueue(queue, 30); 
-    enqueue(queue, 40); 
-  
-    printf("%d dequeued from queue\n\n", dequeue(queue)); 
-  
-    printf("Front item is %d\n", front(queue)); 
-    printf("Rear item is %d\n", rear(queue)); 
-  
-    return 0; 
-} 
+
+#ifndef hw3Header
+#define hw3Header
+
+class DataItem {
+    
+    public:
+    
+    	int k; // Key
+    	char d; // satellite datum, assume each datum is only a char.
+    
+    public:
+    	DataItem() {}; // HINT: implement this...    
+    	DataItem( int newKey, char newData ) { // HINT: implement this...
+            k = newKey;
+            d = newData;
+    	    
+    	}
+    	~DataItem(); // HINT: implement this...
+};
+
+class ArrayQueue { // Please implement a queue using an array...
+    // HINT: Add some fields here...
+
+    // HINT: implement all of these...
+    void enqueue( DataItem x ); // Insert a data item to front. If the array is full, double its size.
+    DataItem dequeue(); // Return a DataItem from tail. If the array is less than half-full, half its size.
+    bool isEmpty(); // Determine if the queue is empty
+	bool isFull();  // Determine if the queue is full
+	DataItem getBack(); // Get the last data item of the queue without removing it
+	int getSize(); // Return the current number of data items in the queue
+	int getCapacity(); // Return the size of the array that keeps the data items (must be at least as large as the number of data items in the queue)
+};
+
+class ArrayCircularQueue { // Please implement a circular queue using an array...
+    // HINT: Add some fields here...
+
+    // HINT: implement all of these...
+    void enqueue( DataItem x ); // Insert a data item to front. If the array is full, double its size.
+    DataItem dequeue(); // Return a DataItem from tail. If the array is less than half-full, half its size.
+    bool isEmpty(); // Determine if the queue is empty
+    bool isFull();  // Determine if the queue is full
+	DataItem getBack(); // Get the last data item of the queue without removing it
+    int getSize(); // Return the current number of data items in the queue
+    int getCapacity(); // Return the size of the array that keeps the data items (must be at least as large as the number of data items in the queue)
+};
+
+class ArrayStack { // Please implement a stack using an array...
+	// HINT: Add some fields here...
+
+	// HINT: implement all of these...
+	void push(int x) { // Insert a data item to top. If the array is full, double its size.
+        if (top ==  n-1)
+	}
+	DataItem pop(); // Return a DataItem from top. If the array is less than half-full, half its size.
+	bool isEmpty(); // Determine if the stack is empty
+	DataItem getTop(); // Get the last data item of the stack without removing it
+	int getSize(); // Return size of the array that keeps the data items (must be at least as large as the number of data items in the stack)
+};
+
+class TwoStackQueue { // Please implement a queue using two stacks (you CANNOT access array elements directly; instead, you can only call the methods of the previous ArrayStack class)
+	// HINT: Add some fields here...
+
+	// HINT: implement all of these...
+	void enqueue();
+	DataItem dequeue();
+	bool isEmpty();
+	bool isFull();
+	DataItem getBack();
+	int getSize();
+};
+
+#endif
+
+
+int main()
+{
+    cout<<"Hello World";
+
+    return 0;
+}
