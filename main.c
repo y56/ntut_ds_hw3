e81f87121fc01a0f724b0e08ed5a10b51c1f95e6
y56 <36190560+y56@users.noreply.github.com>
2019-05-19 18:20:31 +0800

    Update main.c

--- main.c
+++ main.c
@@ -5,80 +5,110 @@ using namespace std;
 #ifndef hw3Header
 #define hw3Header
 
-class DataItem {
-    
-    public:
-    
-        int k; // Key
-    	char d; // satellite datum, assume each datum is only a char.
-    
-    public:
-    	DataItem() {}; // HINT: implement this...    
-    	DataItem( int newKey, char newData ) { // HINT: implement this...
-            k = newKey;
-            d = newData;
-    	    
-    	}
-    	~DataItem(); // HINT: implement this...
+class DataItem
+{
+public:
+
+    int k;			                    // Key
+    char d;			                    // satellite datum, assume each datum is only a char.
+
+    DataItem ()                         // constructor w/o input, using some default values 
+    {				        
+        k = 0;
+        d = '0';
+    }
+    DataItem (int newKey, char newData)	// constructor w/ input
+    {
+        k = newKey;
+        d = newData;
+    }
+    ~DataItem ()                        // destructor
+    {
+        // type 'int' argument given to 'delete', expected pointer 
+        // ???
+    }; 
 };
 
-class ArrayQueue { // Please implement a queue using an array...
+class ArrayQueue
+{   // Please implement a queue using an array...
     // HINT: Add some fields here...
 
     // HINT: implement all of these...
-    void enqueue( DataItem x ); // Insert a data item to front. If the array is full, double its size.
-    DataItem dequeue(); // Return a DataItem from tail. If the array is less than half-full, half its size.
-    bool isEmpty(); // Determine if the queue is empty
-	bool isFull();  // Determine if the queue is full
-	DataItem getBack(); // Get the last data item of the queue without removing it
-	int getSize(); // Return the current number of data items in the queue
-	int getCapacity(); // Return the size of the array that keeps the data items (must be at least as large as the number of data items in the queue)
+  void enqueue (DataItem x);	// Insert a data item to front. If the array is full, double its size.
+  DataItem dequeue ();		    // Return a DataItem from tail. If the array is less than half-full, half its size.
+  bool isEmpty ();		        // Determine if the queue is empty
+  bool isFull ();		        // Determine if the queue is full
+  DataItem getBack ();		    // Get the last data item of the queue without removing it
+  int getSize ();		        // Return the current number of data items in the queue
+  int getCapacity ();		    // Return the size of the array that keeps the data items (must be at least as large as the number of data items in the queue)
 };
 
-class ArrayCircularQueue { // Please implement a circular queue using an array...
+class ArrayCircularQueue
+{   // Please implement a circular queue using an array...
     // HINT: Add some fields here...
 
     // HINT: implement all of these...
-    void enqueue( DataItem x ); // Insert a data item to front. If the array is full, double its size.
-    DataItem dequeue(); // Return a DataItem from tail. If the array is less than half-full, half its size.
-    bool isEmpty(); // Determine if the queue is empty
-    bool isFull();  // Determine if the queue is full
-	DataItem getBack(); // Get the last data item of the queue without removing it
-    int getSize(); // Return the current number of data items in the queue
-    int getCapacity(); // Return the size of the array that keeps the data items (must be at least as large as the number of data items in the queue)
+    void enqueue (DataItem x);	// Insert a data item to front. If the array is full, double its size.
+    DataItem dequeue ();		// Return a DataItem from tail. If the array is less than half-full, half its size.
+    bool isEmpty ();		    // Determine if the queue is empty
+    bool isFull ();		        // Determine if the queue is full
+    DataItem getBack ();		// Get the last data item of the queue without removing it
+    int getSize ();		        // Return the current number of data items in the queue
+    int getCapacity ();		    // Return the size of the array that keeps the data items (must be at least as large as the number of data items in the queue)
 };
 
-class ArrayStack { // Please implement a stack using an array...
-	// HINT: Add some fields here...
+class ArrayStack
+{   // Please implement a stack using an array...
+    // HINT: Add some fields here...
+public:
+    int top = 0;
+    
+    ArrayStack (int arrayLen)  // construct array 
+    {
+        int stack[arrayLen] = { };
+        top = 0;
+    }
 
-	// HINT: implement all of these...
-	void push(int x) { // Insert a data item to top. If the array is full, double its size.
-        if (top ==  n-1)
-	}
-	DataItem pop(); // Return a DataItem from top. If the array is less than half-full, half its size.
-	bool isEmpty(); // Determine if the stack is empty
-	DataItem getTop(); // Get the last data item of the stack without removing it
-	int getSize(); // Return size of the array that keeps the data items (must be at least as large as the number of data items in the stack)
+
+
+    // HINT: implement all of these...
+    void push (DataItem dataItem)	// Insert a data item to top. If the array is full, double its size.
+    {
+        if (top == n - 1)
+        {
+            cout << "Stack is full. Overflow condition! Shall double the array!";
+        }
+        else
+        {
+	        top++;
+	        stack[top] = key;
+        }
+    }
+    DataItem pop ();		// Return a DataItem from top. If the array is less than half-full, half its size.
+    bool isEmpty ();		// Determine if the stack is empty
+    DataItem getTop ();		// Get the last data item of the stack without removing it
+    int getSize ();		    // Return size of the array that keeps the data items (must be at least as large as the number of data items in the stack)
 };
 
-class TwoStackQueue { // Please implement a queue using two stacks (you CANNOT access array elements directly; instead, you can only call the methods of the previous ArrayStack class)
-	// HINT: Add some fields here...
+class TwoStackQueue
+{	    // Please implement a queue using two stacks (you CANNOT access array elements directly; instead, you can only call the methods of the previous ArrayStack class)
+        // HINT: Add some fields here...
 
-	// HINT: implement all of these...
-	void enqueue();
-	DataItem dequeue();
-	bool isEmpty();
-	bool isFull();
-	DataItem getBack();
-	int getSize();
+        // HINT: implement all of these...
+    void enqueue ();
+    DataItem dequeue ();
+    bool isEmpty ();
+    bool isFull ();
+    DataItem getBack ();
+    int getSize ();
 };
 
 #endif
 
 
-int main()
+int main ()
 {
-    cout<<"Hello World";
+    cout << "Hello World";
 
     return 0;
 }
